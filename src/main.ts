a26fe173d4946f5f6c012e57a4dbf3a2f4169240
intKuka <lepkafill@gmail.com>
2024-06-15 14:14:11 +0300

    initial create

---
+++ src/main.ts
@@ -0,0 +1,12 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const port = process.env.PORT || 3000;
+  const app = await NestFactory.create(AppModule);
+
+  await app.listen(port, () => {
+    console.log('Server is running at ' + port);
+  });
+}
+bootstrap();
